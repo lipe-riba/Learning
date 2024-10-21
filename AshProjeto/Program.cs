7942db3ad42bb4ee2b137222dc3110020de7e575
lipe-riba <felipebribeiro_n@hotmail.com>
2024-10-21 15:48:00 -0300

    cardinalPoints variable renamed to cardinalPointsArgs

--- AshProjeto/Program.cs
+++ AshProjeto/Program.cs
@@ -8,29 +8,28 @@ namespace AshProjeto
     {
         public static void Main(string[] args)
         {
-            string cardinalPoints = "";
+            string cardinalPointsArgs = "";
  
             Console.WriteLine("Type the sequence of movements to catch the pokemons:\n" +
                               "Key: N (North), S (South), E (East), O (West)" +
                               "\n\n" +
-                              "eg: NSNSNNEEEEOOO\n" +
-                              $"{cardinalPoints}");
+                              "eg: NSNSNNEEEEOOO\n");
 
             //If it is coming from prompt command.
             if (args.Length > 0)
             {
                 //Change to uppercase chars If the user has inputted the coordinates with lowercase chars
-                cardinalPoints = args[0].ToUpper();
-                Console.WriteLine(cardinalPoints);
+                cardinalPointsArgs = args[0].ToUpper();
+                Console.WriteLine(cardinalPointsArgs);
             }
             else
             {
                 //If there are no coordinates
-                cardinalPoints = Console.ReadLine();
+                cardinalPointsArgs = Console.ReadLine();
             }
  
             //If it has no initial movements
-            if (cardinalPoints.Length == 0)
+            if (cardinalPointsArgs.Length == 0)
             {
                 MessageService.Show("Type the movements.");
                 return;
@@ -38,7 +37,7 @@ namespace AshProjeto
 
             //Counting all movements in typed coordinates
             CardinalPointsService model = new CardinalPointsService(new CardinalPoints());
-            int total = model.GetTotalPokemonsByCardinalPoint(cardinalPoints);
+            int total = model.GetTotalPokemonsByCardinalPoint(cardinalPointsArgs);
 
             //If has some invalid movements
             if (total == -1)
