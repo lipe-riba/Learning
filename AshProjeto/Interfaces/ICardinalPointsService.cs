6a23d9043c7462eaf3507ce54ec98c76dd963675
lipe-riba <felipebribeiro_n@hotmail.com>
2024-10-21 15:23:00 -0300

    First commit

---
+++ AshProjeto/Interfaces/ICardinalPointsService.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace AshProjeto.Interfaces
+{
+    public interface ICardinalPointsService
+    {
+        int GetTotalPokemonsByCardinalPoint(string cardinalPoint);
+    }
+}
